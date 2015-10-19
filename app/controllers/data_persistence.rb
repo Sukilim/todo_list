d667f37fa772686c1c9e44050c4f3b9bb5c5b25f
Sukilim <suki.fonxin@gmail.com>
2015-10-19 14:45:15 +0800

    Update data_persistence.rb

--- app/controllers/data_persistence.rb
+++ app/controllers/data_persistence.rb
@@ -10,20 +10,32 @@ require_relative '../models/task'
 class TodoApplication <ActiveRecord::Base
 
 	def self.list
-		puts "Here are the list of things to do: "
-		p a = Task.all
+		puts "All task: "
+		task = Task.all
+		task.each_with_index do |task, index|
+			if task.complete == 0
+				puts "#{index + 1}. #{task.name} [Not Done]"
+			else 
+				puts "#{index + 1}. #{task.name} [Done]"
+			end
+		end
 	end
 
-	def self.add(name)
-	 a = Task.create(name: "#{name}", complete: 0)
-	 a.save
-	 puts "Task: #{name} is added"
+	def self.add(string)
+	task = Task.create(name: string, complete: 0)
+	task.save
+	puts "Task: #{string} is added"
 	end
 
-	def self.delete(num)
-		puts "Task is deleted"
-		p a = Task.find_by(id: num)
-		a.destroy
+	def self.delete(string)
+		# puts "Task is deleted"
+		# p a = Task.find_by(id: num)
+		# a.destroy
+	task = Task.all
+	index = string.to_i
+	task = task[index - 1]
+	task.destroy
+	puts "Task is deleted: #{task.name}"
 	end
 
 	def self.update(num, status)
@@ -34,11 +46,17 @@ class TodoApplication <ActiveRecord::Base
 		p a
 	end
 
-	def self.complete(num)
-		a = Task.find_by(id: num)
-		a.update(complete: 1)
-		puts "Task mark as completed: "
-		p Task.find(num)
+	def self.complete(string)
+		# a = Task.find_by(id: num)
+		# a.update(complete: 1)
+		# puts "Task mark as completed: "
+		# p Task.find(num)
+		task = Task.all
+		index = string.to_i
+		task = task[index -1]
+		task.update(complete: 1)
+		task.save
+		puts "Task mark as completed: #{task.name}"
 	end
 end
 
@@ -60,4 +78,5 @@ end
 #test complete:
 # TodoApplication.complete(4)
 # p Task.find(4)
+
 
