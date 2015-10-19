e31466e99de7361d07961a743447524f27eb7995
Sukilim <suki.fonxin@gmail.com>
2015-10-19 14:43:39 +0800

    Update todo.rb

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require_relative 'config/application'
 require_relative 'app/models/task'
+require_relative 'app/controllers/data_persistence'
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
 
@@ -10,36 +11,13 @@ string = ARGV.join(" ")
 
 case command
 when 'list'
-	puts "All task: "
-	task = Task.all
-	task.each_with_index do |task, index|
-		if task.complete == 0
-			puts "#{index + 1}. #{task.name} [Not Done]"
-		else 
-			puts "#{index + 1}. #{task.name} [Done]"
-		end
-	end
+	TodoApplication.list
 when 'add'
-	task = Task.create(name: string, complete: 0)
-	task.save
-	puts "Task: #{string} is added"
+	TodoApplication.add(string)
 when 'delete'
-	# task = Task.all
-	# puts "Task is deleted"
-	# 	p a = Task.find(string.to_i)
-	# 	a.destroy
-	task = Task.all
-	index = string.to_i
-	task = task[index - 1]
-	task.destroy
-	puts "Task is deleted: #{task.name}"
+	TodoApplication.delete(string)
 when 'complete'
-	task = Task.all
-	index = string.to_i
-	task = task[index -1]
-	task.update(complete: 1)
-	task.save
-	puts "Task mark as completed: #{task.name}"
+	TodoApplication.complete(string)
 else 
 	puts "Invalid Command."
 end
